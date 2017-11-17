033b697dc540e375e404111472a8ce10e4b0d6b5
ASE-Student <benediktschepperle@dhcp-10-177-4-106.dynamic.eduroam.mwn.de>
2017-11-17 11:34:19 +0100

    Hello User hinzugefügt

---
+++ src/HelloUser.java
@@ -0,0 +1,11 @@
+
+public class HelloUser {
+
+	String username;
+	
+	
+	public void greetuser () {
+		System.out.println("Hello " + username);
+	}
+	
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,22 @@
 
+import java.io.*;
+
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException
+	{
 		// TODO Auto-generated method stub
+		
+		HelloUser user = new HelloUser();
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		System.out.print("Geben sie den Usernamen ein: ");
+		String eingabe = br.readLine();
+		user.username = eingabe;
+		
+		
 		System.out.println("Hello ASE2017 - How are you? :)");
+		user.greetuser();
 	}
 
 }
